80855e2242d647e3c0a792157620a03380830318
Chris Dalla Santa <chris.dallasanta@gmail.com>
2019-01-05 01:10:06 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -5,10 +5,10 @@ class Anagram
   end
 
   def match(arr)
-    main_word_sorted = @word.sort
+    main_word_sorted = @word.char.sort.join
     matches = []
     arr.each do |arr_word|
-      arr_word_sorted = arr_word.sort
+      arr_word_sorted = arr_word.char.sort.join
       if arr_word_sorted == main_word_sorted
         matches << arr_word
       end
